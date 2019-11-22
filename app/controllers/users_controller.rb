828981affaa8fa4c741f19969b14cb788057dd2d
Reynard van den Berg <reyvdb@gmail.com>
2019-11-22 18:39:03 +0800

    add editing and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     
     def new
+        @user = User.new
     end
     
     def create
@@ -13,7 +14,21 @@ class UsersController < ApplicationController
         else
             render :new
         end
+    end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+
+        if @user.update(user_params)
+            redirect_to  new_user_path
+        else
+            render :edit
+        end 
 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create] #resources helper method creates default routes, these can be specified with only:
+  resources :users, only: [:new, :create, :edit, :update] #resources helper method creates default routes, these can be specified with only:
 
 end
